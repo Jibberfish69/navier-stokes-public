52308321d258268022a6036bc4e442b052dbfecf
Thomas Birnie <research@users.noreply.github.com>
2026-06-07 20:29:59 -0600

    NS research revision
    Source revision: dbd40cd341669020d931aa6769830e4ac40a4b58

--- problems/navier-stokes/tools/promote_repo_depth_to_pdf.rb
+++ problems/navier-stokes/tools/promote_repo_depth_to_pdf.rb
@@ -265,7 +265,8 @@ def sync_submission_export_status!
     overrides: {
       PaperFactoryRuntime::NightlyNsSubmissionReadinessAgreement::EXPORT_STATUS_PATH => provisional_payload
     },
-    ignore_export_submission_ready_flag: true
+    ignore_export_submission_ready_flag: true,
+    ignore_child_paper_repo_dirty: true
   )
   readiness_evidence = readiness_evidence.merge(
     "pdf_tracks_present" => tracks.values.all? { |track| track["present"] == true },
